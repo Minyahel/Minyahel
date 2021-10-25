eef08e73162f4c100498d65322e40b9dd80e5023
Minyahel Fekadu Haile <minyahelfekadu13@gmail.com>
2021-10-25 17:25:06 +0200

    Updates

---
+++ precache-manifest.4237bf2449beb3f4e207bbe19032883f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67636381cd62df418b0a540c3fec4948",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9370259403e9044c9fc1",
+    "url": "./static/css/2.a2524b97.chunk.css"
+  },
+  {
+    "revision": "5fdce142b5974e006c6b",
+    "url": "./static/css/main.9bcb1388.chunk.css"
+  },
+  {
+    "revision": "9370259403e9044c9fc1",
+    "url": "./static/js/2.25154fcb.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.25154fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fdce142b5974e006c6b",
+    "url": "./static/js/main.f285521b.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/minyahel/precache-manifest.c2166197371beee9dbeffa78523ff408.js"
+  "./precache-manifest.4237bf2449beb3f4e207bbe19032883f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minyahel/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
